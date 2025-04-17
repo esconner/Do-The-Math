19c4c8c6f01345f7b28c383daf716e27a0ef3b9d
Eric Sean Conner <esconner@me.com>
2025-04-17 10:18:01 +0200

    Add command for appending value of an evaluated expression .

--- Do The Math.novaextension/Scripts/main.js
+++ Do The Math.novaextension/Scripts/main.js
@@ -8,8 +8,7 @@ exports.deactivate = function() {
     // Clean up state before the extension is deactivated
 }
 
-
-nova.commands.register("do-the-math.compute", (editor) => {
+nova.commands.register("do-the-math-and-replace.compute", (editor) => {
     // Replaces the selected text with the evaluated string.
     var selectedRanges = editor.selectedRanges.reverse();
     editor.edit(function(e) {
@@ -19,6 +18,23 @@ nova.commands.register("do-the-math.compute", (editor) => {
                 var newText = "" + mexp.eval(text);
                 e.delete(range);
                 e.insert(range.start, newText);
+            }
+            catch(e) {
+                console.error(e.message);
+            }
+        }
+    });
+});
+
+nova.commands.register("do-the-math.compute", (editor) => {
+    // Appends the evaluated string to the expression.
+    var selectedRanges = editor.selectedRanges.reverse();
+    editor.edit(function(e) {
+        for (var range of selectedRanges) {
+            var text = editor.getTextInRange(range);
+            try {
+                var newText = " = " + mexp.eval(text);
+                e.insert(range.end, newText)
             }
             catch(e) {
                 console.error(e.message);
